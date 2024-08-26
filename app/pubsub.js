7b8f1d3f87c71ac875bdb1650232133804af4656
Shashank5412 <patilshashank54@gmail.com>
2024-08-26 13:30:39 +0530

    first commit

---
+++ app/pubsub.js
@@ -0,0 +1,76 @@
+const PubNub = require('pubnub');
+
+const credentials = {
+  publishKey: 'pub-c-f996248b-52f4-43a1-978c-c97bf7db922f',
+  subscribeKey: 'sub-c-434e7787-aa1e-47a7-a647-4a8451527bb8',
+  secretKey: 'sec-c-M2NjMWFlNzAtOTAzOS00MGU2LWJhMzctYTM4NmNkNzU0Mzhj'
+};
+
+const CHANNELS = {
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
+  TRANSACTION: 'TRANSACTION'
+};
+
+class PubSub {
+  constructor({ blockchain, transactionPool }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+
+    this.pubnub = new PubNub(credentials);
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+    this.pubnub.addListener(this.listener());
+  }
+
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    switch(channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedMessage, true, () => {
+          this.transactionPool.clearBlockchainTransactions({
+             chain: parsedMessage
+          });
+        });
+        break;
+      case CHANNELS.TRANSACTION:
+        this.transactionPool.setTransaction(parsedMessage);
+        break;
+      default:
+        return;
+    }
+  }
+
+
+  listener() {
+    return {
+      message: messageObject => {
+        const { channel, message } = messageObject;
+
+        this.handleMessage(channel, message);
+      }
+    };
+  }
+
+  publish({ channel, message}) {
+    this.pubnub.publish({ channel, message });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
+    })
+  }
+}
+
+module.exports = PubSub;
